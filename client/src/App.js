7339c5daf03d643f00a2708c07fab10938d5b4bb
Mariia Pinda <55147172+Mariia-Pinda@users.noreply.github.com>
2022-06-18 00:03:02 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, {useContext, useEffect, useState} from 'react';
 import AppRouter from "./components/AppRouter";
-import {Navbar, Spinner} from "react-bootstrap";
-import {observer} from "mobx-react-lite";
+import {Spinner} from "react-bootstrap";
 import {Context} from "./index";
 import {check} from "./http/userAPI";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
     const {user} = useContext(Context)
@@ -13,6 +13,10 @@ function App() {
         check().then(data => {
             user.setUser(true)
             user.setIsAuth(true)
+        }).catch(e => {
+            if (window.location.pathname!=="/registration") {
+                window.location.pathname="/registration"
+            }
         }).finally(() => setLoading(false))
     }, [])
 
@@ -20,9 +24,9 @@ function App() {
         return <Spinner animation={"grow"}/>
     }
     return (
-        <div>
+        <BrowserRouter>
             <AppRouter/>
-        </div>
+        </BrowserRouter>
     );
 }
 
